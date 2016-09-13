440f53f25011250ba058db4b2b0ad27685a24fac
jacob <you@example.com>
2016-09-13 17:35:26 +0200

    week 2 ex 2_3 done

--- week2/ex_2_3.py
+++ week2/ex_2_3.py
@@ -23,22 +23,38 @@ data_clean = []
 for line in data:
     # words_list = line['request_text'].encode('utf-8').split()
     # words_list = re.findall(r"[\w']+", line['request_text'].encode('utf-8'))
-    words_list = re.sub("[^a-zA-Z]", " ", line['request_text'].encode('utf-8'))
-    data_clean.append([w for w in words_list.lower().split() if not w in stops])
-    for word in words_list.split():
-        word_dict.add(word.lower())
+    if line['request_text'] != '':
+        words_list = re.sub("[^a-zA-Z]", " ", line['request_text'])
+        data_clean.append([w for w in words_list.lower().split() if not w in stops])
+        for word in words_list.split():
+            word_dict.add(word.lower())
 
 meaningful_words = [w for w in word_dict if not w in stops]
-# print len(meaningful_words)
-# print meaningful_words
-# print data_clean[0:1]
 
 vectorizer = CountVectorizer(analyzer = "word",   \
                              tokenizer = None,    \
                              preprocessor = None, \
                              stop_words = None,   \
-                             max_features = 5000)
+                             max_features = 200000)
+data_clean_train = []
 
-train_data_features = vectorizer.fit_transform(data_clean)
+for line in data_clean:
+    data_clean_train.append(" ".join(line))
+# -------
+train_data_features = vectorizer.fit_transform(data_clean_train)
 train_data_features = train_data_features.toarray()
 print train_data_features.shape
+# -------
+# vocab = vectorizer.get_feature_names()
+# print vocab
+# -------
+# dist = np.sum(train_data_features, axis=0)
+#
+# for tag, count in zip(vocab, dist):
+#     print count, tag
+# -------
+
+with open('features', 'w') as file_:
+    for line in train_data_features:
+        file_.write(str(line))
+        file_.write("\n")
