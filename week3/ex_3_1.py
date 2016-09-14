b0bd196f7e8c504226ab8383da7c9efdb3843641
jacob <you@example.com>
2016-09-14 17:14:10 +0200

    week3 ex 1 done

--- week3/ex_3_1.py
+++ week3/ex_3_1.py
@@ -1,4 +1,36 @@
+#!/usr/bin/python
 import numpy as np
+import sys
+from numpy.linalg import solve
 
-dupa = 123
-dupa = 323
+
+bla = [
+    [1,2,3,4],
+    [6,9,12,7],
+    [2,0,9,10]
+]
+
+filename = sys.argv[1]
+
+with open(filename, 'r') as f:
+    data = f.read().splitlines()
+
+# print data
+#
+# big = [map(int, x) for line in data for x in line.split(',')]
+#
+# print big
+
+big = []
+small = []
+for line in data:
+    for num in line.split(','):
+        small.append(int(num))
+    big.append(small)
+    small = []
+
+A = np.array(big)[0:3,0:3]
+b = np.array(big)[:,3:4]
+
+
+print solve(A,b)
