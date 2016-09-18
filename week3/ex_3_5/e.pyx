2dd7ba9bf27fb713f8cb6e53af12dae7ef49de07
jacob <you@example.com>
2016-09-18 13:45:28 +0200

    week 3 ex 3.5 improved, moved l variable from float to int

--- week3/ex_3_5/e.pyx
+++ week3/ex_3_5/e.pyx
@@ -1,10 +1,10 @@
 def f(rep):
-	cdef int i
-	cdef float summ,l
+	cdef int i, l
+	cdef float summ
 	i = 0
 	while (i<=rep):
 		summ=0.
-		l=1.
+		l=1
 		while l<=10000:
 			summ += 1. / l ** 2
 			l += 1
