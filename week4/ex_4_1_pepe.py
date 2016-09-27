cbe24ac5bc5507eb6573a825f7722445be009d6f
jacob <aa>
2016-09-27 22:51:22 +0200

    week 4 doneee

--- week4/ex_4_1_pepe.py
+++ week4/ex_4_1_pepe.py
@@ -5,7 +5,7 @@ from scipy import *
 import numpy as np
 
 
-filename = "data_10points_10dims.dat"
+filename = "data_1000points_1000dims.dat"
 
 spr_matrix = pickle.load(open(filename, 'r'))
 dense_matrix = ss.csr_matrix(spr_matrix).toarray()
@@ -14,14 +14,17 @@ dense_matrix = ss.csr_matrix(spr_matrix).toarray()
 def dist(set_1,set_2):
     intersection = (set_1 & set_2).tolist().count(1)
     union = (set_1 | set_2).tolist().count(1)
+    # print "set_1 " , set_1
+    # print "set_2 " , set_2
+    # print "union" , union
     return 1 - intersection / float(union)
 
 # -------- --------------- --------
 
-def regionQuery(data,one_point_data):
+def regionQuery(one_point_data):
     neighbourhood = []
 
-    for index in data:
+    for index in points_info:
         point_to_compare = points_info[index]['point_data']
 
         if dist(point_to_compare,one_point_data) <= eps:
@@ -39,33 +42,38 @@ def expandCluster(point_info,neighborPts):
 
         if index not in visited_indices:
             visited_indices.append(index)
-            neighborPts_prime = regionQuery(neighborPts,point_info)
+            neighborPts_prime = regionQuery(points_info[index]['point_data'])
 
             if len(neighborPts_prime) >= minPts:
-                neighborPts = neighborPts + neighborPts_prime
+                neighborPts += neighborPts_prime
+                # neighborPts = list(set(neighborPts).intersection(neighborPts_prime))
+        if index not in added_to_any_cluster:
+            clusters[current_cluster_index].append(index)
+            added_to_any_cluster.append(index)
 
-        if points_info[index]['cluster'] == 'NaN':
-            points_info[index]['cluster'] = current_cluster_index
+        # if points_info[index]['cluster'] == 'NaN':
+        #     points_info[index]['cluster'] = current_cluster_index
 
 # -------- Prepare lookup point dictionary --------
 points_info = dict()
 
 for ind, matrix_entry in enumerate(dense_matrix):
     points_info[ind] = {'point_index': ind, 'point_data': matrix_entry, 'cluster': "NaN"}
-print "-------- All points info: --------"
-for point in points_info:
-    print points_info[point]
-print "-------- --------------- --------"
+# print "-------- All points info: --------"
+# for point in points_info:
+#     print points_info[point]
+# print "-------- --------------- --------"
 
 # -------- GLOBAL variables --------
 visited_indices = []
 noise_indices = []
 clusters = dict()
 current_cluster_index = 0
+added_to_any_cluster = []
 
 # -------- DBSCAN parameters --------
 minPts = 2
-eps = 0.4
+eps = 0.15
 
 # -------- DBSCAN main --------
 
@@ -73,29 +81,26 @@ for point_index in points_info:
     if point_index in visited_indices:
         continue
 
-    visited_indices.append(point_index)
+    visited_indices.append(point_index) # mark P as visited
 
-    neighborPts = regionQuery(range(0,len(points_info)),points_info[point_index]['point_data'])
+    neighborPts = regionQuery(points_info[point_index]['point_data']) # NeighborPts = regionQuery(P, eps)
 
-    if len(neighborPts) < minPts:
-        noise_indices.append(point_index)
+    if len(neighborPts) < minPts: # if sizeof(NeighborPts) < MinPts
+        noise_indices.append(point_index) # mark P as NOISE
     else:
         current_cluster_index += 1
-        points_info[point_index]['cluster'] = current_cluster_index
+        clusters[current_cluster_index] = []
+        clusters[current_cluster_index].append(point_index)
+        added_to_any_cluster.append(point_index)
         expandCluster(points_info[point_index]['point_data'],neighborPts)
 
-print "-------- All noise indices: --------"
-print noise_indices
+# print "-------- All noise indices: --------"
+# print noise_indices
 
 
-print "-------- Updated points info: --------"
-for point in points_info:
-    print points_info[point]
-print "-------- --------------- --------"
+# print "-------- Updated points info: --------"
+# for point in points_info:
+#     print points_info[point]
+# print "-------- --------------- --------"
 
-distinct_clusters = set()
-
-for bla in points_info:
-    distinct_clusters.add(points_info[bla]['cluster'])
-
-print len(distinct_clusters)
+print len(clusters)
