f952d583d2936a93e1d52b00b8770215fedc0ed1
jacob <aa>
2016-10-14 20:58:40 +0200

    bla

--- week7/ex_7_3/ex_7_3.py
+++ week7/ex_7_3/ex_7_3.py
@@ -1,31 +1,100 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+import collections
 
 class CountTriangles(MRJob):
 
-    def mapper_1(self, _, line):
+    def mapper(self, _, line):
         node_1, node_2 = line.split()
         yield int(node_1), int(node_2)
         yield int(node_2), int(node_1)
 
-    def reducer_1(self, key, values):
-        for node in values:
-            yield key, map(lambda x: x,values)
-
-    def mapper_2(self, key, neighbours):
-        for vertex in neighbours:
-            yield vertex, [key,neighbours]
+    def reducer(self, key_node, connected_nodes):
+        # print "key", key
+        # for bla in values:
+        #     print bla
+        # print "------+"
+        # values_copy = reduce(lambda x, y: x + [y], values, [])
+        connected_nodes_copy = list(connected_nodes)
+        for node in connected_nodes_copy:
+            yield sorted([key_node,node]), connected_nodes_copy
 
     def reducer_2(self, key, values):
-        print key, values
+        # print "key",key
+        # bla = list(values)
+        # print bla
+        # yield key,
+        list_of_nodes_sets = []
+        for node_list in values:
+            list_of_nodes_sets.append(set(node_list))
+        # print list_of_nodes_sets
+        intersec = set.intersection(*list_of_nodes_sets)
+        # print intersec
+        # print "----"
+        # print "bla",list(intersec)
+        yield key,list(intersec)
+
+
+
+    def reducer_3(self, key, values):
+        # print "key",key
+        # bla = list(values)
+        # print bla
+        # pass
+        # node_list = list(key)
+        # for node in values:
+        #     if node !=[]:
+        #         node_list.append(node)
+        # # flatten_list = [val for sublist in node_list for val in sublist]
+        # maybe_a_triangle = sorted(self.flatten(node_list))
+        # print maybe_a_triangle
+        # if len(maybe_a_triangle)==3:
+        #     yield maybe_a_triangle, 1
+        # blabla = list(values)
+        neighbour_nodes = sorted(self.flatten(values))
+        # print neighbour_nodes
+        # keys_list = list(key)
+
+        if len(neighbour_nodes) > 0:
+            for node in neighbour_nodes:
+                triangle = []
+                triangle += key
+                triangle += [node]
+                triangle = sorted(triangle)
+                # print "key", key, "triangle",triangle , "node", node
+                yield triangle, 1
+
+
+    def reducer_4(self,key,val):
+        # print key
+        # for bla in val:
+        #     print bla
+        # print "----"
+        yield "a triangle", 1
+
+    def reducer_5(self,key,val):
+        yield "amount", sum(val)
+
+    def flatten(self,l):
+        for el in l:
+            if isinstance(el, collections.Iterable) and not isinstance(el, basestring):
+                for sub in self.flatten(el):
+                    yield sub
+            else:
+                yield el
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_1,
-                   reducer=self.reducer_1
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer
                    ),
-            MRStep(mapper=self.mapper_2,
-                   reducer=self.reducer_2
+            MRStep(reducer=self.reducer_2
+                   ),
+            MRStep(reducer=self.reducer_3
+                   ),
+            MRStep(reducer=self.reducer_4
+                   ),
+            MRStep(reducer=self.reducer_5
                    )
         ]
 
