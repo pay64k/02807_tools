4b60e7caa34cf8cb3676b3d0938cbc2b4298f38a
jacob <pay64k@gmail.com>
2016-11-05 20:42:57 +0100

    9.2 upd

--- week9/ex_9_2.py
+++ week9/ex_9_2.py
@@ -6,89 +6,99 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import HashingVectorizer
 from datetime import datetime
 
+
+def min_hash(perm,data):
+    print "#---------------------------------------#\n" \
+          "Staring minHash with", perm, "permutations\n" \
+          "#---------------------------------------#"
+    original_array = np.transpose(data)
+
+    # TODO: remove amount restriction
+    amount_of_permutations = perm
+    hash_functions_array = [[0 for i in range(original_array.shape[1])] for ii in range(amount_of_permutations)]
+
+    for permutation in range(amount_of_permutations):
+        permuted_array = np.copy(original_array)
+        np.random.shuffle(permuted_array)
+        permuted_array = np.transpose(permuted_array)
+        row_index = 0
+        for row in permuted_array:
+            column_index = 0
+            for number in row:
+                if number > 0:
+                    hash_functions_array[permutation][row_index] = column_index
+                    break
+                column_index += 1
+            row_index += 1
+        print "Done with permutation #:", permutation, "/", amount_of_permutations
+
+    # each row corresponds to an article
+    hash_functions_array = np.transpose(hash_functions_array)
+
+
+    def jaccard_dist(set_1,set_2):
+        intersection = len(set_1 & set_2)
+        union = len(set_1 | set_2)
+        return 1 - intersection / float(union)
+
+    print "Creating buckets based on Jaccard distance..."
+
+    buckets = {}
+
+    print "#----------------- Done ----------------#"
+
 # ------------- read in entries from all files -------------
 
-data_raw = []
-data_raw_filtered = []
-data_directory = "data"
-data_topics = []
+if __name__ == '__main__':
 
-for file in os.listdir(data_directory):
-    if file.endswith(".json"):
-        data = helpers.read_file(data_directory + "/" + file)
-        data_raw.append(data)
+    data_raw = []
+    data_raw_filtered = []
+    data_directory = "data"
+    data_topics = []
 
-for file in data_raw:
-    for entry in file:
-        if ("topics" in entry) and ("body" in entry):
-            data_raw_filtered.append(entry)
+    for file in os.listdir(data_directory):
+        if file.endswith(".json"):
+            data = helpers.read_file(data_directory + "/" + file)
+            data_raw.append(data)
 
-print "All articles loaded..."
-# print data_raw_filtered[0]
-# ------------------ create bag of words -----------------
+    for file in data_raw:
+        for entry in file:
+            if ("topics" in entry) and ("body" in entry):
+                data_raw_filtered.append(entry)
 
-data_clean = []
+    print "All articles loaded..."
+    # print data_raw_filtered[0]
+    # ------------------ create bag of words -----------------
 
-# TODO: remove amount restriction
-for entry in data_raw_filtered[:5]:
-    words_list = re.sub("[^a-zA-Z]", " ", entry['body'])
-    data_clean.append({"body": [w for w in words_list.lower().split()],
-                       "topics": entry["topics"],
-                       "id": entry["id"]})
+    data_clean = []
 
-# print data_clean[0]
+    # TODO: remove amount restriction
+    for entry in data_raw_filtered[:5]:
+        words_list = re.sub("[^a-zA-Z]", " ", entry['body'])
+        data_clean.append({"body": [w for w in words_list.lower().split()],
+                           "topics": entry["topics"],
+                           "id": entry["id"]})
 
-data_words_only = []
+    # print data_clean[0]
 
-for article in data_clean:
-    data_words_only.append(" ".join(article["body"]))  # glue all words together into a list of strings
+    data_words_only = []
 
-vectorizer = CountVectorizer(analyzer="word",
-                             tokenizer=None,
-                             preprocessor=None,
-                             stop_words='english'
-                             )
+    for article in data_clean:
+        data_words_only.append(" ".join(article["body"]))  # glue all words together into a list of strings
 
-_features = vectorizer.fit_transform(data_words_only)
+    vectorizer = CountVectorizer(analyzer="word",
+                                 tokenizer=None,
+                                 preprocessor=None,
+                                 stop_words='english'
+                                 )
 
-train_data_features_array = _features.toarray()
+    _features = vectorizer.fit_transform(data_words_only)
 
-print "Got all features...", train_data_features_array.shape
+    _features_array = _features.toarray()
 
-# print train_data_features_array
-# ----------------------------------------
+    print "Got all features...", _features_array.shape
 
-original_array = np.transpose(train_data_features_array)
+    # print train_data_features_array
+    # ----------------------------------------
 
-# TODO: remove amount restriction
-amount_of_permutations = 3
-hash_functions_array = [[0 for i in range(original_array.shape[1])] for ii in range(amount_of_permutations)]
-
-print "Starting minHash for", amount_of_permutations, "permutations..."
-
-for permutation in range(amount_of_permutations):
-    permuted_array = np.copy(original_array)
-    np.random.shuffle(permuted_array)
-    permuted_array = np.transpose(permuted_array)
-    row_index = 0
-    for row in permuted_array:
-        column_index = 0
-        for number in row:
-            if number > 0:
-                hash_functions_array[permutation][row_index] = column_index
-                break
-            column_index += 1
-        row_index += 1
-    print "Done with permutation #:", permutation, "/", amount_of_permutations
-
-# each row corresponds to an article
-hash_functions_array = np.transpose(hash_functions_array)
-
-
-def jaccard_dist(set_1,set_2):
-    intersection = len(set_1 & set_2)
-    union = len(set_1 | set_2)
-    return 1 - intersection / float(union)
-
-print "Creating buckets based on Jaccard distance..."
-
+    min_hash(3,_features_array)
