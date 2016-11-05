2be46a7c53419ea2dccbc72c8b17bc283faa9eb8
jacob <pay64k@gmail.com>
2016-11-05 16:37:40 +0100

    9.2 upd

--- week9/ex_9_2.py
+++ week9/ex_9_2.py
@@ -13,8 +13,7 @@ data_raw_filtered = []
 data_directory = "data"
 data_topics = []
 
-# TODO: remove amount restriction
-for file in os.listdir(data_directory)[:1]:
+for file in os.listdir(data_directory):
     if file.endswith(".json"):
         data = helpers.read_file(data_directory + "/" + file)
         data_raw.append(data)
@@ -27,21 +26,21 @@ for file in data_raw:
 print data_raw_filtered[0]
 # ------------------ create bag of words -----------------
 
-data_clean = {}
+data_clean = []
 
 # TODO: remove amount restriction
-for entry in data_raw_filtered[:2]:
+for entry in data_raw_filtered[:3]:
     words_list = re.sub("[^a-zA-Z]", " ", entry['body'])
-    data_clean[entry["id"]] = {"body": [w for w in words_list.lower().split()],
+    data_clean.append({"body": [w for w in words_list.lower().split()],
                                "topics": entry["topics"],
-                               "id": entry["id"]}
+                               "id": entry["id"]})
 
-print data_clean["7001"]
+print data_clean[0]
 
 data_words_only = []
 
 for article in data_clean:
-    data_words_only.append(" ".join(data_clean[article]["body"]))  # glue all words together into a list of strings
+    data_words_only.append(" ".join(article["body"]))  # glue all words together into a list of strings
 
 vectorizer = CountVectorizer(analyzer="word",
                              tokenizer=None,
@@ -59,8 +58,13 @@ print train_data_features_array
 # ----------------------------------------
 # ----------------------------------------
 
-# Array form the previous
-minhashArray = train_data_features_array
+minhashArray = np.transpose(train_data_features_array)
+print minhashArray, "\n^ up there is minhashArray"
+
+amount_of_permutations = 3
+
+for hash in range(amount_of_permutations):
+    np.take(minhashArray, np.random.permutation(minhashArray.shape[0]), axis=0, out=minhashArray)
 
 # get the row and columns of arrray
 r = np.shape(minhashArray)[0]
@@ -71,11 +75,10 @@ c = np.shape(minhashArray)[1]
 phiSize = r
 permutationArray = np.array(range(phiSize))
 np.random.shuffle(permutationArray)
-# ----------------------------------------
 
 Result = np.zeros((r, c))
 # for creat result I iterate trough rows and each time I shuffle permutationArrya and base of the array I create
-# a result which obtain base of the first occurance of the 1  (I checked with three articles and not changing permutationArray
+# a result which obtain base of the first occurance of the 1 I checked with three articles and not changing permutationArray
 # and the result was reasonable)
 for rMinhash in range(r):
     np.random.shuffle(permutationArray)
@@ -87,7 +90,7 @@ for rMinhash in range(r):
                 Result[rMinhash][Col] = int(placeFinder)
 
 # I show the result her to see how the matrix look like after permutation
-print Result
+# print Result
 
 # now I make a bukket list with the similarity in thier columns
 bucket = []
@@ -107,6 +110,6 @@ for word in listOfTheWords:
         bucketList.remove(bucket)
     bucketList.append(bucket)
 
-print "\n\nBuckets:\n\n"
-for article in bucketList:
-    print article, "\n\n"
+# print "\n\nBuckets:\n\n"
+# for article in bucketList:
+#     print article, "\n\n"
