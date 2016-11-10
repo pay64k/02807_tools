19f42f09ef7166f30a633f408f066664177a16b9
jacob <pay64k@gmail.com>
2016-11-10 14:44:38 +0100

    final_ass_upd

--- final_assignment/helpers.py
+++ final_assignment/helpers.py
@@ -1,24 +1,55 @@
 import csv
 
-english_variations = set()
-lets_say_its_english = 0
 
-with open("IMDB_files_link/_filtered_data/language.filtered") as data_file:
-    reader = csv.reader(data_file, delimiter='\r')
+def language_fun():
+    english_variations = set()
+    lets_say_its_english = 0
+
+    with open("IMDB_files_link/_filtered_data/language.filtered") as data_file:
+        reader = csv.reader(data_file, delimiter='\r')
+        for line in reader:
+            # line variable here is a list of strings, so we join it into one string
+            full_line = " ".join(line)
+            # partition returns 3-tuple: (part_before_delimiter, delimiter, part_after_delimiter)
+            parted = full_line.partition("\t")
+            title = parted[0]
+            # in the part_after_delimiter we delete all \t characters
+            language = parted[2].replace("\t","")
+            if language.startswith("English") and language != "English":
+            # if "English" in language or "english" in language:
+                english_variations.add(language)
+                lets_say_its_english +=1
+
+    for eng in english_variations:
+        print eng
+    print len(english_variations)
+    print lets_say_its_english
+
+
+# with open("IMDB_files_link/directors.list") as data_file:
+#     reader = csv.reader(data_file, delimiter='\n')
+#     for line in reader:
+#         print "line" ,line
+#         print "--------------------------"
+
+bla = []
+
+with open("IMDB_files_link/directors.list") as data_file:
+    reader = csv.reader(data_file, delimiter='\n')
     for line in reader:
-        # line variable here is a list of strings, so we join it into one string
         full_line = " ".join(line)
-        # partition returns 3-tuple: (part_before_delimiter, delimiter, part_after_delimiter)
-        parted = full_line.partition("\t")
-        title = parted[0]
-        # in the part_after_delimiter we delete all \t characters
-        language = parted[2].replace("\t","")
-        if language.startswith("English") and language != "English":
-        # if "English" in language or "english" in language:
-            english_variations.add(language)
-            lets_say_its_english +=1
+        if len(full_line) != 0:
+            bla.append(full_line)
 
-for eng in english_variations:
-    print eng
-print len(english_variations)
-print lets_say_its_english
+# for a in bla[:100]:
+#     print a
+
+print bla[11]
+print bla[12]
+print bla[13]
+
+# ok = []
+# with open("IMDB_files_link/directors.list", "r") as data_file:
+#     for line in data_file:
+#         if line == " ":
+#             print "bla"
