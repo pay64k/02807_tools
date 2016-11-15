ef865a3cbce957d0e465921c2354915cbb84ed6d
jacob <aa>
2016-11-15 10:24:32 +0100

    upd

--- final_assignment/fill_from_wiki.py
+++ final_assignment/fill_from_wiki.py
@@ -30,24 +30,34 @@ with open('imdb_dataset_v6.empty_for_wiki') as csvfile:
 
 
 def compute_jaccard_index(list_1, list_2):
-    set_1 = set(list_1)
-    set_2 = set(list_2)
+    set_1 = set(list_1.split())
+    set_2 = set(list_2.split())
     return len(set_1.intersection(set_2)) / float(len(set_1.union(set_2)))
 
 movies_with_no_budget = []
+wiki_no_info_movies = []
+wiki_ok = []
+
 
 for title in movies:
     if movies[title]["budget"] == "no_info":
+        full_title = title
         title_no_year = title.partition("(")[0]
-        movies_with_no_budget.append(title_no_year)
+        movies_with_no_budget.append([title_no_year,full_title])
 
-for title in movies_with_no_budget[11:12]:
-    search_results = wikipedia.search(title)
+for title in movies_with_no_budget[:20]:
+
+    title_no_year = title[0]
+    full_title = title[1]
+
+    search_results = wikipedia.search(title_no_year)
 
     search_results = [unicodedata.normalize('NFKD', x).encode('ascii','ignore') for x in search_results]
 
+    print "search_results for", title_no_year, search_results
+
     for result in search_results:
-        if compute_jaccard_index(title,result) >= 1.0:
+        if compute_jaccard_index(title_no_year,result) >= 0.9:
             current_query = result
             break
         if "film" in result:
@@ -56,14 +66,30 @@ for title in movies_with_no_budget[11:12]:
         else:
             current_query = "no_results"
 
-    print "----current_query:", current_query , "/ for movie: ", title
+    print "\tcurrent_query:", current_query , "/ for movie: ", title_no_year, "\n"
 
     if current_query != "no_results":
-        movie_page = wikipedia.page(current_query)
-        print movie_page
-        print movie_page.sections
-    # print line,"found:",wikipedia.search(line)
+        movie_page = wptools.page(current_query,silent=True).get_parse()
 
-# first look if there is exact title
-#     if yes then look in that article
-#     if not look for search that returned 'film' in article name
+        if movie_page.infobox is not None:
+            print "infobox for title:", title_no_year
+            print "\t", movie_page.infobox, "\n"
+
+            if "budget" in movie_page.infobox:
+
+                if movie_page.infobox["budget"] == "":
+                    wiki_no_info_movies.append(["empty_info", full_title])
+                else:
+                    budget = movie_page.infobox["budget"]
+                    wiki_ok.append([budget, full_title])
+
+            else:
+                wiki_no_info_movies.append(["no_info", full_title])
+
+        else:
+            wiki_no_info_movies.append(["no_info_box", full_title])
+    else:
+        wiki_no_info_movies.append(["no_info_query", full_title])
+
+for line in wiki_no_info_movies:
+    print line
