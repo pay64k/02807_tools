d86068a56bb5ae3e709a1d5013416136d32ec1d5
jacob <aa>
2016-11-15 08:50:06 +0100

    upd

--- final_assignment/fill_from_wiki.py
+++ final_assignment/fill_from_wiki.py
@@ -1,5 +1,5 @@
-import csv, wikipedia, unicodedata
-
+import csv, wikipedia, unicodedata, wptools
+from sklearn.metrics import jaccard_similarity_score
 
 movies = {}
 
@@ -28,6 +28,12 @@ with open('imdb_dataset_v6.empty_for_wiki') as csvfile:
         }
 # print movies["Get Him to the Greek (2010)"]
 
+
+def compute_jaccard_index(list_1, list_2):
+    set_1 = set(list_1)
+    set_2 = set(list_2)
+    return len(set_1.intersection(set_2)) / float(len(set_1.union(set_2)))
+
 movies_with_no_budget = []
 
 for title in movies:
@@ -35,10 +41,13 @@ for title in movies:
         title_no_year = title.partition("(")[0]
         movies_with_no_budget.append(title_no_year)
 
-for title in movies_with_no_budget[:10]:
+for title in movies_with_no_budget[11:12]:
     search_results = wikipedia.search(title)
+
+    search_results = [unicodedata.normalize('NFKD', x).encode('ascii','ignore') for x in search_results]
+
     for result in search_results:
-        if result == title:
+        if compute_jaccard_index(title,result) >= 1.0:
             current_query = result
             break
         if "film" in result:
@@ -47,6 +56,12 @@ for title in movies_with_no_budget[:10]:
         else:
             current_query = "no_results"
 
+    print "----current_query:", current_query , "/ for movie: ", title
+
+    if current_query != "no_results":
+        movie_page = wikipedia.page(current_query)
+        print movie_page
+        print movie_page.sections
     # print line,"found:",wikipedia.search(line)
 
 # first look if there is exact title
